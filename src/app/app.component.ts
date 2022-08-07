bd389d3d471fa8b7a0e9211bba25a9099575d17e
Lukas Andersson <lukas.s.andersson@gmail.com>
2022-08-07 12:21:29 +0200

    Root to standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  imports: [RouterModule],
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'OverlayClock';
