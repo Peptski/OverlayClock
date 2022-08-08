781da33512cec0aa8c37444864c0789fada481fd
Lukas Andersson <lukas.s.andersson@gmail.com>
2022-08-08 14:19:51 +0200

    Initial settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ClockComponent } from './clock/clock.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @Component({
-  imports: [RouterModule, ClockComponent],
+  imports: [RouterModule, ClockComponent, SettingsComponent],
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
