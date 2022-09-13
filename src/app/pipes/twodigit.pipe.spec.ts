cdf08cee6ba40b1dc54870bdd8d7223352d66289
Lukas Andersson <lukas.s.andersson@gmail.com>
2022-09-13 22:10:36 +0200

    Delete twodigit.pipe.spec.ts

--- src/app/pipes/twodigit.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TwodigitPipe } from './twodigit.pipe';
-
-describe('TwodigitPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TwodigitPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
